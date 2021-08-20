dfe9ead21a943e569fd5ef300f92fb8849332aec
PCalape <86319081+PCalape@users.noreply.github.com>
2021-08-20 11:52:48 +0800

    Add backend files via upload

---
+++ routes/posts.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+//Gets back all the posts
+router.get('/', async (req,res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//Submits a post
+router.post('/', async (req,res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+    }catch(err){
+        res.json({message: err});s
+    }
+});
+
+//Specific post
+router.get('/:postID', async (req,res) => {
+    try{
+    const post = await Post.findById(req.params.postID);
+    res.json(post);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//Delete Post
+router.delete('/:postID', async (req,res) => {
+    try {
+    const removedPost = await Post.remove({_id: req.params.postID})
+    res.json(removedPost);
+    }catch(err) {
+        res.json({message: err}); 
+    }
+});
+
+//Update post
+router.patch('/:postID', async (req,res) => {
+    try {
+    const updatedPost = await Post.updateOne(
+        {_id: req.params.postID},
+        { $set: {title: req.body.title}});
+    res.json(updatedPost);
+    }catch(err) {
+        res.json({message: err}); 
+    }
+});
+
+module.exports = router;
